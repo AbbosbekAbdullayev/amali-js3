f05401778acc03d4e5122876b10f4e9df53660d4
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-29 07:34:58 +0500

    NUMBERLADIK

--- mein.js
+++ mein.js
@@ -2,12 +2,15 @@
 let yosh = prompt("yoshingizni kiriting");
 yosh = Number(yosh);
 
-if(yosh!==nan){
+if(yosh){
     if(yosh>=16) {
         console.log('siz pasport olasiz')
     }
     else{
         console.log("siz pasport olmaysiz")
     }
+}
+else{
+    console.log("togri qiymat kirit!")
 }
 console.log(yosh)
