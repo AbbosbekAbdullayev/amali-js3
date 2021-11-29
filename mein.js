92a7e5c5fd14e9d697adc1318bcc0df4ca7348ee
Abbos Abdullayev <91402157+AbbosbekAbdullayev@users.noreply.github.com>
2021-11-29 20:31:11 +0500

    smit

--- mein.js
+++
@@ -1,9 +0,0 @@
-let image = document.getElementById('img');
-let atugma = document.getElementById('atugma');
-let btugma = document.getElementById('btugma');
-atugma.onclick=function(){
-    image.style.display='none'
-}
-btugma.onclick=function(){
-    image.style.display='block'
-}
